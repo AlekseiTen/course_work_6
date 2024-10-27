0972e0c6ebb9e2db990b11dd3a5d8f511dedd244
Aleksei Ten <amailcn@mail.ru>
2024-10-27 21:20:30 +0300

    написал круд для клиента, форму , и шаблоны

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from mailing.views import index, ClientListView, ClientCreateView
+from mailing.views import index, ClientListView, ClientCreateView, ClientUpdateView, ClientDeleteView, ClientDetailView
 
 app_name = "mailing"
 
 urlpatterns = [
-    path("", index),
+    path("", index, name="index"),
     path("clients/", ClientListView.as_view(), name="client_list"),
     path("clients/create/", ClientCreateView.as_view(), name="client_create"),
+    path("clients/<int:pk>/update/", ClientUpdateView.as_view(), name="client_update"),
+    path("clients/<int:pk>/delete/", ClientDeleteView.as_view(), name="client_delete"),
+    path("clients/<int:pk>/", ClientDetailView.as_view(), name="client_detail"),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -17,9 +17,9 @@ class ClientListView(ListView):
     context_object_name = "clients"
 
 
-# class ClientDetailView(DetailView):
-#     model = Client
-#     template_name = "mailing/client_detail.html"
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = "mailing/client_detail.html"
 
 
 class ClientCreateView(CreateView):
@@ -28,13 +28,13 @@ class ClientCreateView(CreateView):
     success_url = reverse_lazy("mailing:client_list")
 
 
-# class ClientUpdateView(UpdateView):
-#     model = Client
-#     form_class = ClientForm
-#     success_url = reverse_lazy("mailing:client_list")
-#
-#
-# class ClientDeleteView(DeleteView):
-#     model = Client
-#     template_name = "mailing/client_confirm_delete.html"
-#     success_url = reverse_lazy("mailing:client_list")
+class ClientUpdateView(UpdateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy("mailing:client_list")
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = "mailing/client_confirm_delete.html"
+    success_url = reverse_lazy("mailing:client_list")
