1800368be79c2d40d199383c50392be19ebebb14
Aleksei Ten <amailcn@mail.ru>
2024-10-27 20:12:12 +0300

    написал круд для клиента, форму , и шаблоны

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from mailing.views import index
+from mailing.views import index, ClientListView, ClientCreateView
 
 app_name = "mailing"
 
 urlpatterns = [
     path("", index),
+    path("clients/", ClientListView.as_view(), name="client_list"),
+    path("clients/create/", ClientCreateView.as_view(), name="client_create"),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,4 +1,40 @@
+
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from mailing.forms import ClientForm
+from mailing.models import Client
+
 
 def index(request):
     return render(request, 'mailing/base.html')
+
+
+class ClientListView(ListView):
+    model = Client
+    template_name = "mailing/client_list.html"
+    context_object_name = "clients"
+
+
+# class ClientDetailView(DetailView):
+#     model = Client
+#     template_name = "mailing/client_detail.html"
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy("mailing:client_list")
+
+
+# class ClientUpdateView(UpdateView):
+#     model = Client
+#     form_class = ClientForm
+#     success_url = reverse_lazy("mailing:client_list")
+#
+#
+# class ClientDeleteView(DeleteView):
+#     model = Client
+#     template_name = "mailing/client_confirm_delete.html"
+#     success_url = reverse_lazy("mailing:client_list")
