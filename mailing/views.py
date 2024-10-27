64f1b653430e3bbb7d8cd0fe773f54ab3a833dd4
Aleksei Ten <amailcn@mail.ru>
2024-10-27 22:46:16 +0300

    создал шаблоны для сообщений

--- mailing/views.py
+++ mailing/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from mailing.forms import ClientForm
-from mailing.models import Client
+from mailing.forms import ClientForm, MessageForm, MailingForm
+from mailing.models import Client, Message, Mailing
 
 
 def index(request):
@@ -42,59 +42,59 @@ class ClientDeleteView(DeleteView):
 
 # CRUD для сообщений
 class MessageListView(ListView):
-    model = Client
-    template_name = "mailing/client_list.html"
-    context_object_name = "clients"
+    model = Message
+    template_name = "mailing/message_list.html"
+    context_object_name = "messages"
 
 
 class MessageDetailView(DetailView):
-    model = Client
-    template_name = "mailing/client_detail.html"
+    model = Message
+    template_name = "mailing/message_detail.html"
 
 
 class MessageCreateView(CreateView):
-    model = Client
-    form_class = ClientForm
-    success_url = reverse_lazy("mailing:client_list")
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy("mailing:message_list")
 
 
 class MessageUpdateView(UpdateView):
-    model = Client
-    form_class = ClientForm
-    success_url = reverse_lazy("mailing:client_list")
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy("mailing:message_list")
 
 
 class MessageDeleteView(DeleteView):
-    model = Client
-    template_name = "mailing/client_confirm_delete.html"
-    success_url = reverse_lazy("mailing:client_list")
+    model = Message
+    template_name = "mailing/message_confirm_delete.html"
+    success_url = reverse_lazy("mailing:message_list")
 
 
 # CRUD для рассылки
 class MailingListView(ListView):
-    model = Client
-    template_name = "mailing/client_list.html"
-    context_object_name = "clients"
+    model = Mailing
+    template_name = "mailing/mailing_list.html"
+    context_object_name = "mailings"
 
 
 class MailingDetailView(DetailView):
-    model = Client
-    template_name = "mailing/client_detail.html"
+    model = Mailing
+    template_name = "mailing/mailing_detail.html"
 
 
 class MailingCreateView(CreateView):
-    model = Client
-    form_class = ClientForm
-    success_url = reverse_lazy("mailing:client_list")
+    model = Mailing
+    form_class = MailingForm
+    success_url = reverse_lazy("mailing:mailing_list")
 
 
 class MailingUpdateView(UpdateView):
-    model = Client
-    form_class = ClientForm
-    success_url = reverse_lazy("mailing:client_list")
+    model = Mailing
+    form_class = MailingForm
+    success_url = reverse_lazy("mailing:mailing_list")
 
 
 class MailingDeleteView(DeleteView):
-    model = Client
-    template_name = "mailing/client_confirm_delete.html"
-    success_url = reverse_lazy("mailing:client_list")
+    model = Mailing
+    template_name = "mailing/mailing_confirm_delete.html"
+    success_url = reverse_lazy("mailing:mailing_list")
