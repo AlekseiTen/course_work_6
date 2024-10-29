007d5775f0149f400c263686a7b683d524b50e79
Aleksei Ten <amailcn@mail.ru>
2024-10-29 21:27:52 +0300

    создал комманду по созданию суперюзера

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "users.User"
 
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 465
--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,7 +1,4 @@
-
-
 from django.core.management import BaseCommand
-
 from users.models import User
 
 
