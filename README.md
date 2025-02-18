71a11e0ff95fd3f521721f6d7bb256862976ae64
Aleksei Ten <amailcn@mail.ru>
2025-02-18 18:05:23 +0300

    вынес логин пароль для суперюзера в шаблон

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 Для запуска шедулера необходимо вызвать команду: python manage.py runapscheduler
 
-для создания админа команда: python manage.py csu admin@mail.com пароль 123
+
--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         user = User.objects.create(email=os.getenv("SU_EMAIL"))
-        user.set_password(os.getenv("SU_PASS"))
+        user.set_password(os.getenv("SU_PASSWORD"))
         user.is_active = True
         user.is_staff = True
         user.is_superuser = True
