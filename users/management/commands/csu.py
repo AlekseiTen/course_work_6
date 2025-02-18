0840885294c05c3bfcd74c5e4bb872dd8ec3c638
Aleksei Ten <amailcn@mail.ru>
2025-02-18 17:46:47 +0300

    вынес логин пароль для суперюзера в шаблон

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,11 +1,14 @@
+import os
+
 from django.core.management import BaseCommand
 from users.models import User
-
+from dotenv import load_dotenv
+load_dotenv()
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        user = User.objects.create(email="admin@mail.com")
-        user.set_password("123")
+        user = User.objects.create(email=os.getenv("SU_EMAIL"))
+        user.set_password(os.getenv("SU_PASS"))
         user.is_active = True
         user.is_staff = True
         user.is_superuser = True
